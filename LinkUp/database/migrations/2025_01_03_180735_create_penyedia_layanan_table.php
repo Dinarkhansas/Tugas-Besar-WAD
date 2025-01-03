78d5b3ae5a715db1a8597e5aa11c2c285e6654a9
MeizonRura <148075370+MeizonRura@users.noreply.github.com>
2025-01-04 01:11:18 +0700

    migrate

---
+++ LinkUp/database/migrations/2025_01_03_180735_create_penyedia_layanan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penyedia_layanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->integer('umur');
+            $table->string('alamat');
+            $table->integer('kontak');
+            $table->string('deskripsi');
+            $table->string('jenis_layanan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penyedia_layanan');
+    }
+};
