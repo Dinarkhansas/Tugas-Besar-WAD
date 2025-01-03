9c6f132d28b5f44e5ec3ef493f2476860583269d
Dinarkhansas <salsabiladinarkhansa@gmail.com>
2025-01-04 00:20:56 +0700

    tambahan dinar

--- LinkUp/database/migrations/2025_01_03_171136_create_pemesanan_table.php
+++ LinkUp/database/migrations/2025_01_03_171136_create_pemesanan_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('pemesanan', function (Blueprint $table) {
             $table->id();
-            $table->string('password');
             $table->string('jenis_layananan');
             $table->string('harga');
             $table->timestamps();
