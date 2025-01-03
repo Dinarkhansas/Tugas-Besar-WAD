202e9f541c6d8a6ed458012fb80185a7d2d817bc
Dinarkhansas <salsabiladinarkhansa@gmail.com>
2025-01-04 00:19:57 +0700

    tambahin migrasi dinar

---
+++ LinkUp/database/migrations/2025_01_03_171136_create_pemesanan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemesanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('password');
+            $table->string('jenis_layananan');
+            $table->string('harga');
+            $table->timestamps();
+
+            $table->foreign('id_penyedia_layanan')->references('id')->on('penyedia_layanan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemesanan');
+    }
+};
